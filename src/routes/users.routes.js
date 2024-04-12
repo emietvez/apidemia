57e56c8dcccff5fb1e9007023d74aaf62eef783b
Emi Estevez <eestevez@unbcorp.cl>
2024-04-11 21:03:46 -0300

    Change routes users

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,10 +3,6 @@ import { createUser, getUsers } from "../controllers/users.controller.js";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.render("index")
-})
-
 router.get('/users', getUsers)
 router.post('/user', createUser)
 
